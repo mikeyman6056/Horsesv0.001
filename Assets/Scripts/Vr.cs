4314a54ad65da448d57c8f68f8a0b9a98615cd1d
mikeyman6056 <mikeyman2450@gmail.com>
2023-03-28 12:35:35 +0800

    commit2

---
+++ Assets/Scripts/Vr.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Vr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public XRNode inpuytSource;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
